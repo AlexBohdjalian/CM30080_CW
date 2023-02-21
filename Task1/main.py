04f03d44acdbb12108174738b46a406011e689e0
AlexBohdjalian <44300792+AlexBohdjalian@users.noreply.github.com>
2023-02-21 15:42:32 +0000

    Implement clustering and fix line detection parameters

--- Task1/main.py
+++ Task1/main.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from sklearn.cluster import KMeans
 
 RED = '\u001b[31m'
 GREEN = '\u001b[32m'
@@ -17,46 +18,44 @@ results = [(item.split(',')[0], int(item.split(',')[1].strip())) for item in res
 
 for file_name, actual_angle in results:
     img_path = 'Task1/assets/' + str(file_name)
-    img = cv2.imread(img_path, cv2.COLOR_BGR2GRAY)
+    img = cv2.imread(img_path, cv2.COLOR_RGB2GRAY)
 
-    _, img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)
-    edges = cv2.Canny(img, 100, 255, 2)
-    lines = cv2.HoughLines(edges, 1, np.pi/180, 85)
+    img = cv2.GaussianBlur(img, (5, 5), 0)
+
+    edges = cv2.Canny(img, 50, 150, apertureSize=3)
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50, minLineLength=10, maxLineGap=250)
 
     try:
-        # Draw the detected lines for visual aid
-        # for line in lines:
-        #     for rho,theta in line:
-        #         a = np.cos(theta)
-        #         b = np.sin(theta)
-        #         x0 = a*rho
-        #         y0 = b*rho
-        #         x1 = int(x0 + 3000*(-b))
-        #         y1 = int(y0 + 3000*(a))
-        #         x2 = int(x0 - 3000*(-b))
-        #         y2 = int(y0 - 3000*(a))
-        #         cv2.line(img,(x1,y1),(x2,y2),(0,255,0),1)
-        # cv2.imshow(file_name, img)
-        # cv2.waitKey(1)
-        line1 = lines[0][0]
-        line2 = lines[1][0]
+        for line in lines:
+            cv2.line(img, (line[0][0], line[0][1]), (line[0][2], line[0][3]), (0, 255, 0))
+        cv2.imshow('img', img)
+        cv2.waitKey(2000)
 
-        # For HoughLines
-        angle = np.arctan2(np.sin(line2[1]-line1[1]), np.cos(line2[1]-line1[1]))
+        # K-means clustering
+        lines = lines.reshape(-1, 4)
+        kmeans = KMeans(n_clusters=2, random_state=0, n_init='auto').fit(lines)
+        labels = kmeans.labels_
+        line_label_dict = {}
+
+        line1 = None
+        line2 = None
+        for line, label in zip(lines, labels):
+            line_label_dict[label] = line_label_dict.get(label, []) + [np.round(line)]
+
+        line1 = np.mean(line_label_dict[0], axis=0)
+        line2 = np.mean(line_label_dict[1], axis=0)
+
+        print(line1, line2)
+
+        delta_x = line2[2] - line2[0]
+        delta_y = line2[3] - line2[1]
+        angle = np.arctan2(delta_y, delta_x) - np.arctan2(line1[3] - line1[1], line1[2] - line1[0])
         angle = round(abs(angle * 180 / np.pi))
-
-        # For HoughLinesP
-        # delta_x = line2[2] - line2[0]
-        # delta_y = line2[3] - line2[1]
-        # angle = np.arctan2(delta_y, delta_x) - np.arctan2(line1[3] - line1[1], line1[2] - line1[0])
-        # angle = round(abs(angle * 180 / np.pi))
 
         message = f'The angle in {file_name} is \t{angle} deg \t{GREEN}{actual_angle} deg{NORMAL}'
         if angle == actual_angle:
             print_correct(message)
         else:
             print_wrong(message)
-            # print('Lines...')
-            # print(lines)
     except:
         print('Error occured')
