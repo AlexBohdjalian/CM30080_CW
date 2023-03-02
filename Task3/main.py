7a7a9e9ff80cc954cc21e75a663161c03e472d06
AlexBohdjalian <44300792+AlexBohdjalian@users.noreply.github.com>
2023-03-02 17:13:25 +0000

    WIP Sift feature matching and case testing

--- Task3/main.py
+++ Task3/main.py
@@ -1,9 +1,36 @@
-# some imports
+import cv2
+import numpy as np
 
 def training_process(training_data):
     print('NOT IMPLEMENTED')
     raise Exception('Function not implemented')
 
-def feature_detection(image_path):
-    print('NOT IMPLEMENTED')
-    raise Exception('Function not implemented')
+def feature_detection(image_path, image_paths_to_match_against):
+    original_query_image = cv2.imread(image_path)
+    query_image = cv2.cvtColor(original_query_image, cv2.COLOR_BGR2GRAY)
+
+    sift = cv2.SIFT_create()
+    query_kp, query_desc = sift.detectAndCompute(query_image, None)
+
+    bf = cv2.BFMatcher()
+
+    found_features = []  # Create an empty list to store the features found in the training set
+
+    for img_path in image_paths_to_match_against:
+        current_image = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+
+        current_kp, current_desc = sift.detectAndCompute(current_image, None)
+
+        matches = sorted(bf.match(query_desc, current_desc), key=lambda x: x.distance)
+
+        # Get the best match and check if it is within a threshold distance
+        if matches[0].distance < 50:
+            found_features.append([feature_name_from_path(img_path), (), ()])
+
+    # cv2.imshow('img', original_query_image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    return found_features
+
+def feature_name_from_path(img_path):
+    return img_path[img_path.find('-')+1:img_path.find('.png')]
--- Task3/test.py
+++ Task3/test.py
@@ -27,13 +27,14 @@ def read_no_rotations_dataset(dir):
             feature_coord2 = eval(feature[end_of_tuple + 2:])
 
             all_features.append([feature_class, feature_coord1, feature_coord2])
-        all_data.append([image_file, all_features])
+        all_features.sort(key=lambda x:x[0])
+        all_data.append([dir + 'images/' + image_file, all_features])
 
     return all_data
 
 def read_training_dataset(dir):
     print(f'Reading dataset {dir} ...')
-    return list(os.listdir(dir + 'png/'))
+    return [dir + 'png/' + path for path in os.listdir(dir + 'png/')]
 
 def read_rotations_dataset(dir):
     print(f'Reading dataset {dir} ...')
@@ -54,7 +55,8 @@ def read_rotations_dataset(dir):
             feature_coord2 = eval(feature[end_of_tuple + 2:])
 
             all_features.append([feature_class, feature_coord1, feature_coord2])
-        all_data.append([image_file, all_features])
+        all_features.sort(key=lambda x:x[0])
+        all_data.append([dir + 'images/' + image_file, all_features])
 
     return all_data
 
@@ -67,15 +69,6 @@ except Exception as e:
     print(NORMAL)
     exit()
 
-print('Training...')
-try:
-    training_process(all_training_data)
-except Exception as e:
-    print(RED, 'Unknown error occurred while processing images:', NORMAL, e)
-    print(NORMAL)
-    exit()
-
-
 print('Processing images...')
 try:
     correct = []
@@ -84,14 +77,34 @@ try:
     test_dataset = all_no_rotation_images_and_features
     for image_path, actual_features in test_dataset: # replace this with suitable dataset (or combined)
         try:
-            predicted_features = feature_detection(image_path)
+            predicted_features = sorted(feature_detection(image_path, all_training_data), key = lambda x : x[0])
+            print('For:', image_path)
+            print('Predicted:', predicted_features)
+            print('Actual   :', actual_features)
+            print()
+            # print('Match between query descriptor {} and target descriptor {}: distance = {}'.format(
+            #     predicted_features.queryIdx,
+            #     predicted_features.trainIdx,
+            #     predicted_features.distance
+            # ))
 
-            if predicted_features == actual_features:
+            # TODO: change this to match class names and loosely match bounding boxes?
+            actual_feature_names = [feature[0] for feature in actual_features]
+            is_correct = True
+            for feature in predicted_features:
+                if feature[0] not in actual_feature_names:
+                    is_correct = False
+                    break
+
+            if is_correct:
                 correct.append(image_path)
+                # print('Correct!!!')
             else:
                 wrong.append(image_path)
-        except:
-            print(RED, 'Uncaught error occurred while testing image:', image_path, NORMAL)
+                # print('IN-Correct!!!')
+
+        except Exception as e:
+            print(RED, 'Unknown error occurred while testing image:', image_path, NORMAL, e)
             errors.append(image_path)
 
     BLUE = '\u001b[34m'
