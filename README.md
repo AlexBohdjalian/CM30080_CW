9458f3e2d67ab634c1d39849320ba3defed787cc
AlexBohdjalian <44300792+AlexBohdjalian@users.noreply.github.com>
2023-03-14 13:33:45 +0000

    Progress so far

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 Computer Vision Group CW
 
 ### Task1: Measuring the angle
+TODO: Describe to examiner how the code works and how to use it on their own data
 
+### Task2: 
+TODO: Describe to examiner how the code works and how to use it on their own data
 
+### Task3: SIFT Feature Matching
+TODO: Describe to examiner how the code works and how to use it on their own data
--- Task3/test.py
+++ Task3/test.py
@@ -76,73 +76,79 @@ except Exception as e:
     print(RED, 'Error while reading datasets:', NORMAL, traceback.format_exc())
     exit()
 
-# TODO: might need to increase this for better performance
-# TODO: sift should be invariant of image rotation so why does this improve performance?
-directions = 8
-angles = [360 / directions * i for i in range(directions)] # up, tr, right, br, down, bl, left, tl
-tmp_file_dir = 'TmpRotated/'
-rotated_training_dataset = []
-print('Applying rotation to training set ...')
-try:
-    # TODO: decide if keep this or generate new each time for robustness
-    if os.path.exists(task3_training_data_dir + tmp_file_dir):
-        shutil.rmtree(task3_training_data_dir + tmp_file_dir)
-    os.makedirs(task3_training_data_dir + tmp_file_dir)
+# # TODO: might need to increase this for better performance
+# # TODO: sift should be invariant of image rotation so why does this improve performance?
+# directions = 8
+# angles = [360 / directions * i for i in range(directions)] # up, tr, right, br, down, bl, left, tl
+# tmp_file_dir = 'TmpRotated/'
+# rotated_training_dataset = []
+# print('Applying rotation to training set ...')
+# try:
+#     # TODO: decide if keep this or generate new each time for robustness
+#     if os.path.exists(task3_training_data_dir + tmp_file_dir):
+#         shutil.rmtree(task3_training_data_dir + tmp_file_dir)
+#     os.makedirs(task3_training_data_dir + tmp_file_dir)
 
-    for training_image in all_training_data:
-        image = cv2.imread(training_image)
-        for angle in angles:
-            height, width = image.shape[:2] # image shape has 3 dimensions
-            image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared to shape
+#     for training_image in all_training_data:
+#         image = cv2.imread(training_image)
+#         for angle in angles:
+#             height, width = image.shape[:2] # image shape has 3 dimensions
+#             image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared to shape
 
-            rotated_image = cv2.getRotationMatrix2D(image_center, angle, 1.)
+#             rotated_image = cv2.getRotationMatrix2D(image_center, angle, 1.)
 
-            # rotation calculates the cos and sin, taking absolutes of those.
-            abs_cos = abs(rotated_image[0,0]) 
-            abs_sin = abs(rotated_image[0,1])
+#             # rotation calculates the cos and sin, taking absolutes of those.
+#             abs_cos = abs(rotated_image[0,0]) 
+#             abs_sin = abs(rotated_image[0,1])
 
-            # find the new width and height bounds
-            bound_w = int(height * abs_sin + width * abs_cos)
-            bound_h = int(height * abs_cos + width * abs_sin)
+#             # find the new width and height bounds
+#             bound_w = int(height * abs_sin + width * abs_cos)
+#             bound_h = int(height * abs_cos + width * abs_sin)
 
-            # subtract old image center (bringing image back to origo) and adding the new image center coordinates
-            rotated_image[0, 2] += bound_w/2 - image_center[0]
-            rotated_image[1, 2] += bound_h/2 - image_center[1]
+#             # subtract old image center (bringing image back to origo) and adding the new image center coordinates
+#             rotated_image[0, 2] += bound_w/2 - image_center[0]
+#             rotated_image[1, 2] += bound_h/2 - image_center[1]
 
-            # rotate image with the new bounds and translated rotation matrix
-            rotated_image = cv2.warpAffine(image, rotated_image, (bound_w, bound_h), borderValue=(255,255,255))
+#             # rotate image with the new bounds and translated rotation matrix
+#             rotated_image = cv2.warpAffine(image, rotated_image, (bound_w, bound_h), borderValue=(255,255,255))
 
-            rotated_image_name = training_image.replace('/Training/png/', f'/Training/{tmp_file_dir}{round(angle)}deg_')
-            cv2.imwrite(rotated_image_name, rotated_image)
-            rotated_training_dataset.append(rotated_image_name)
-except Exception as e:
-    if os.path.exists(task3_training_data_dir + tmp_file_dir):
-        shutil.rmtree(task3_training_data_dir + tmp_file_dir)
-    print(RED, 'Error while applying rotation to training dataset:', NORMAL, traceback.format_exc())
-    exit()
+#             rotated_image_name = training_image.replace('/Training/png/', f'/Training/{tmp_file_dir}{round(angle)}deg_')
+#             cv2.imwrite(rotated_image_name, rotated_image)
+#             rotated_training_dataset.append(rotated_image_name)
+# except Exception as e:
+#     if os.path.exists(task3_training_data_dir + tmp_file_dir):
+#         shutil.rmtree(task3_training_data_dir + tmp_file_dir)
+#     print(RED, 'Error while applying rotation to training dataset:', NORMAL, traceback.format_exc())
+#     exit()
 
-all_training_data = rotated_training_dataset
+# all_training_data = rotated_training_dataset
 
 try:
     params_list = [{'nfeatures': nf, 'nOctaveLayers': nl, 'contrastThreshold': ct, 'edgeThreshold': et, 'sigma': s, 'matchThreshold': mf}
-        for nf in [0]
-        for nl in [2, 3, 4, 5]
-        for ct in [0.01, 0.05, 0.09]
-        for et in [5, 10, 15, 20]
-        for s in [1.2, 1.6, 2.0]
-        for mf in np.arange(45.0, 55.0, 0.25)
+        for nf in [0] # 0 means no limit, usually in 1000s
+        for nl in [3]
+        for ct in [0.04]
+        for et in [10]
+        for s in [1.6]
+        for mf in [50]
     ]
     # with rotated training dataset:
     # no rotations: 90% acc, 2  false pos, 0 false neg
     # rotations   : 40% acc, 12 false pos, 1 false neg
     # both        : 65% acc, 14 false pos, 1 false neg
+    # with non rotated training dataset:
+    # no rotations: 85% acc, 0  false pos, 3 false neg
+    # rotations   : 5% acc,  false pos,  false neg
+
     # observations: if nOctaveLayers is anything but 2 it performs quite bad, 
-    params_list = [{'nfeatures': 0, 'nOctaveLayers': 2, 'contrastThreshold': 0.01, 'edgeThreshold': 15, 'sigma': 2.0, 'matchThreshold': 50}]
+    # params_list = [{'nfeatures': 0, 'nOctaveLayers': 2, 'contrastThreshold': 0.01, 'edgeThreshold': 15, 'sigma': 2.0, 'matchThreshold': 50}]
 
     best_acc = 0
     best_params = {}
     start_t = time.time()
-    test_dataset = all_no_rotation_images_and_features# + all_rotation_images_and_features
+    # test_dataset = all_no_rotation_images_and_features
+    # test_dataset = all_rotation_images_and_features
+    test_dataset = all_no_rotation_images_and_features + all_rotation_images_and_features
     print(f'There are {len(test_dataset)} images to classify ...')
     for i in range(len(params_list)):
         print(f'Grid-search progress: {i + 1}/{len(params_list)}\t...', end='\r')
