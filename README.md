9b586bfbe4a9716f62064dae93e0d66bf6ac3eea
AlexBohdjalian <44300792+AlexBohdjalian@users.noreply.github.com>
2023-03-17 21:16:47 +0000

    Re-format for marker

--- README.md
+++ README.md
@@ -8,4 +8,9 @@ TODO: Describe to examiner how the code works and how to use it on their own dat
 TODO: Describe to examiner how the code works and how to use it on their own data
 
 ### Task3: SIFT Feature Matching
-TODO: Describe to examiner how the code works and how to use it on their own data
+For this task, we have three files.
+`main.py` contains the functionality for feature matching and an optimised method used for our parameter tuning process.
+
+The file we used to tune our parameters is `hyperopt_tuning_test.py` and does the following...
+
+The file we have created for the markers use is `marker_test.py`. Near the end of the file there are three directories containing paths to the relevant datasets. Please replace these according to your needs, then execute that file to see how the our code performs, with details on...
--- Task3/marker_test.py
+++ Task3/marker_test.py
@@ -9,10 +9,6 @@ RED = '\u001b[31m'
 GREEN = '\u001b[32m'
 NORMAL = '\u001b[0m'
 
-task3_no_rotation_images_dir = 'Task3/Task2Dataset/TestWithoutRotations/'
-task3_rotated_images_dir = 'Task3/Task3Dataset/'
-task3_training_data_dir = 'Task3/Task2Dataset/Training/'
-
 def read_test_dataset(dir, file_ext):
     print(f'Reading test dataset: {dir}')
     image_files = os.listdir(dir + 'images/')
@@ -42,66 +38,74 @@ def read_training_dataset(dir):
     print(f'Reading training dataset: {dir}')
     return [(cv2.imread(dir + 'png/' + path, cv2.IMREAD_GRAYSCALE), path) for path in os.listdir(dir + 'png/')]
 
+def main_process_for_marker_test(test_dataset, training_dataset):
+    try:
+        # TODO: put best parameters here
+        params = {
+            'BFMatcher': {
+                'crossCheck': False,
+                'normType': 4
+            },
+            'RANSAC': {
+                'confidence': 0.9423447867316301,
+                'maxIters': 1500,
+                'ransacReprojThreshold': 5.016592027133279
+            },
+            'ratioThreshold': 0.5011292212940329,
+            'sift': {
+                'contrastThreshold': 0.010041938693454615,
+                'edgeThreshold': 15.355230618863885,
+                'nOctaveLayers': 4,
+                'nfeatures': 2000,
+                'sigma': 1.934389681658647
+            }
+        }
+
+        test_dataset = all_no_rotation_images_and_features + all_rotation_images_and_features
+        correct = 0
+        false_pos = 0
+        false_neg = 0
+        for image, actual_feature_names_set in test_dataset:
+            predicted_features = feature_detection_marker(image, all_training_data, params, show_output=True)
+            predicted_feature_names_set = set([f[0] for f in predicted_features])
+
+            # TODO: as per mark scheme, get True Positives, False Positives, etc.
+            if actual_feature_names_set == predicted_feature_names_set:
+                correct += 1
+                print(GREEN, 'Correct!!!', NORMAL)
+            elif predicted_feature_names_set != actual_feature_names_set:
+                false_pos_dif = predicted_feature_names_set.difference(actual_feature_names_set)
+                false_neg_dif = actual_feature_names_set.difference(predicted_feature_names_set)
+                if any(false_pos_dif):
+                    false_pos += 1
+                if any(false_neg_dif):
+                    false_neg += 1
+                print(RED, 'IN-Correct!!!', NORMAL)
+
+            print('Predicted:', predicted_feature_names_set)
+            print('Actual   :', actual_feature_names_set)
+
+        accuracy = correct * 100 / len(list(test_dataset))
+        print('Accuracy:', accuracy)
+
+    except Exception as e:
+        print(RED, 'Unknown error occurred while processing images:', NORMAL, traceback.format_exc())
+        exit()
+
+# NOTE: For marker, we have assumed that the additional data you have is in the same format as the data give.
+# Please replace the below three directories with your own and then execute this file.
+
+no_rotation_images_dir = 'Task3/Task2Dataset/TestWithoutRotations/'
+rotated_images_dir = 'Task3/Task3Dataset/'
+training_images_dir = 'Task3/Task2Dataset/Training/'
+
 try:
-    all_no_rotation_images_and_features = read_test_dataset(task3_no_rotation_images_dir, '.txt')
-    all_rotation_images_and_features = read_test_dataset(task3_rotated_images_dir, '.csv')
-    all_training_data = read_training_dataset(task3_training_data_dir)
+    all_no_rotation_images_and_features = read_test_dataset(no_rotation_images_dir, '.txt')
+    all_rotation_images_and_features = read_test_dataset(rotated_images_dir, '.csv')
+    all_training_data = read_training_dataset(training_images_dir)
 except Exception as e:
     print(RED, 'Error while reading datasets:', NORMAL, traceback.format_exc())
     exit()
 
-try:
-    # TODO: put best parameters here
-    # TODO: re-write this file for markers to use on their own test data
-    params = {
-        'BFMatcher': {
-            'crossCheck': False,
-            'normType': 4
-        },
-        'RANSAC': {
-            'confidence': 0.9423447867316301,
-            'maxIters': 1500,
-            'ransacReprojThreshold': 5.016592027133279
-        },
-        'ratioThreshold': 0.5011292212940329,
-        'sift': {
-            'contrastThreshold': 0.010041938693454615,
-            'edgeThreshold': 15.355230618863885,
-            'nOctaveLayers': 4,
-            'nfeatures': 2000,
-            'sigma': 1.934389681658647
-        }
-    }
-
-    test_dataset = all_no_rotation_images_and_features + all_rotation_images_and_features
-    correct = 0
-    false_pos = 0
-    false_neg = 0
-    for image, actual_feature_names_set in test_dataset: # replace this with suitable dataset (or combined)
-        predicted_features = feature_detection_marker(image, all_training_data, params, show_output=True)
-        predicted_feature_names_set = set([f[0] for f in predicted_features])
-
-        if actual_feature_names_set == predicted_feature_names_set:
-            correct += 1
-            print(GREEN, 'Correct!!!', NORMAL)
-        elif predicted_feature_names_set != actual_feature_names_set:
-            false_pos_dif = predicted_feature_names_set.difference(actual_feature_names_set)
-            false_neg_dif = actual_feature_names_set.difference(predicted_feature_names_set)
-            if any(false_pos_dif):
-                false_pos += 1
-            if any(false_neg_dif):
-                false_neg += 1
-            print(RED, 'IN-Correct!!!', NORMAL)
-
-        print('Predicted:', predicted_feature_names_set)
-        print('Actual   :', actual_feature_names_set)
-
-    accuracy = correct * 100 / len(list(test_dataset))
-
-except Exception as e:
-    print(RED, 'Unknown error occurred while processing images:', NORMAL, traceback.format_exc())
-    exit()
-
-# # Tidy-up
-# if os.path.exists(task3_training_data_dir + tmp_file_dir):
-#     shutil.rmtree(task3_training_data_dir + tmp_file_dir)
+test_dataset = all_rotation_images_and_features + all_no_rotation_images_and_features
+main_process_for_marker_test(test_dataset, all_training_data)
