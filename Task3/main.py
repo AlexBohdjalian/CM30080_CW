be89acc7e78c3df06184c42e39b39b0c17b80bf3
Nico <nicolas.st@me.com>
2023-04-01 20:14:07 +0300

    extract individual icons before SIFT matching

--- Task3/main.py
+++ Task3/main.py
@@ -1,9 +1,10 @@
 import os
+import cv2
 import time
 import traceback
-
-import cv2
 import numpy as np
+from collections import Counter
+from concurrent.futures import ThreadPoolExecutor
 
 cv2.setRNGSeed(0)
 
@@ -18,6 +19,210 @@ image_info = (
     ('Actual Bounding Box', (0, 20), 1, (0, 255, 0)),
     ('Predicted Bounding Box', (0, 30), 1, (255, 0, 0))
 )
+
+
+def filter_covered_boxes(bounding_boxes):
+    filtered_boxes = []
+
+    for i1, (x1, y1, x2, y2) in enumerate(bounding_boxes):
+        larger = True
+        for i2, (x3, y3, x4, y4) in enumerate(bounding_boxes):
+            if i1 == i2:
+                continue
+            if x3 <= x1 and x4 >= x2 and y3 <= y1 and y4 >= y2:
+                larger = False
+                break
+        if larger:
+            filtered_boxes.append((x1, y1, x2, y2))
+    return filtered_boxes
+
+
+def scale_dimensions(dimension, scale):
+    return round(dimension/scale)
+
+
+def add_border(x_min, y_min, x_max, y_max, width, height, border=1):
+    x_min = max(x_min-border, 0)
+    y_min = max(y_min-border, 0)
+    x_max = min(x_max+border, height)
+    y_max = min(y_max+border, width)
+    return x_min, y_min, x_max, y_max
+
+
+def get_bounding_boxes(image, min_area=500):
+    scale = 10
+    bounding_boxes = []
+    width, height = image.shape[:2]
+    # Resize the image by the scaling factor as it improves the accuracy of the contour detection
+    image = cv2.resize(image, None, fx=scale, fy=scale, interpolation=cv2.INTER_LINEAR)
+    # Binarise and remove noise from the image
+    image = cv2.medianBlur(image, 25)
+    image_res, image = cv2.threshold(image, 240, 255, cv2.THRESH_BINARY_INV)
+    kernel = np.ones((11, 11), np.uint8)
+    image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+    # Find contours in the binary image
+    contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+        x, y, w, h = [scale_dimensions(elem, scale) for elem in cv2.boundingRect(c)]
+        # Ignore small contours that are likely to be noise
+        if w * h < min_area:
+            continue
+        x_min, y_min, x_max, y_max = add_border(x, y, x+w, y+h, width, height)
+        bounding_boxes.append((x_min, y_min, x_max, y_max))
+    return bounding_boxes
+
+
+def segment_icons(image):
+    image_segments = []
+    # get bounding boxes for the image
+    bounding_boxes = get_bounding_boxes(image.copy())
+    # filter out boxes that are entirely covered by a larger bounding box
+    bounding_boxes = filter_covered_boxes(bounding_boxes)
+
+    for x_min, y_min, x_max, y_max in bounding_boxes:
+        image_segment = image[y_min:y_max, x_min:x_max].copy()
+        border = 5
+        padded_image = cv2.copyMakeBorder(
+            image_segment,
+            top=border,
+            bottom=border,
+            left=border,
+            right=border,
+            borderType=cv2.BORDER_CONSTANT,
+            value=(255, 255, 255),
+        )
+        image_segments.append((padded_image, ((x_min, y_min), (x_max, y_max))))
+    return image_segments
+
+
+def sift_detect_and_compute(sift_params, image, resize=None, return_vars=None):
+    # calculate keypoints and descriptors for the image (resize if necessary)
+    sift = cv2.SIFT_create(**sift_params)
+    if resize:
+        image = cv2.resize(image, (resize, resize), interpolation=cv2.INTER_LINEAR)
+    kp, desc = sift.detectAndCompute(image, None)
+    return kp, desc, return_vars
+
+
+def segment_detect_and_compute(sift_params, image, resize, return_vars=None):
+    segments = segment_icons(image.copy())
+    segments_kp_desc = []
+    # calculate keypoins and descriptors for each segment (i.e. individual icon in the image)
+    for image, bounding_box in segments:
+        kp, desc, _ = sift_detect_and_compute(sift_params, image, resize)
+        if desc is not None:
+            segments_kp_desc.append(((kp, desc), bounding_box))
+    return segments_kp_desc, return_vars
+
+
+def draw_bounding_box(image, bounding_box, text, colour=(0, 255, 0)):
+    # draw icons bounding box and predicted name
+    cv2.rectangle(image, bounding_box[0], bounding_box[1], colour, 2)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    text_position = (bounding_box[0][0], bounding_box[0][1] - 5)
+    cv2.putText(image, text, text_position, font, 0.5, colour, 1, cv2.LINE_AA)
+
+
+def feature_detection(training_data, query_data, params, show_output=True):
+    start_time = time.time()
+
+    bf = cv2.BFMatcher(**params['BF'])
+
+    # compute keypoints and descriptors for training data
+    with ThreadPoolExecutor(max_workers=8) as executor1:
+        futures_train = [executor1.submit(sift_detect_and_compute, params['SIFT'], train_image, None, feature) \
+                         for train_image, feature in training_data]
+
+    # collect threading futures and filter out any images that have no descriptors
+    all_training_data_kp_desc = []
+    for future in futures_train:
+        train_kp, train_desc, feature = future.result()
+        if train_desc is None:
+            print(f'No descriptors found for {feature}')
+            continue
+        all_training_data_kp_desc.append((feature, train_kp, train_desc))
+
+    # segment query images into individual icons and computer keypoints and descriptors
+    with ThreadPoolExecutor(max_workers=8) as executor3:
+        futures_query = [executor3.submit(segment_detect_and_compute, params['SIFT'], query_image, params['resizeQuery'], \
+                                          (path, actual_features)) for path, query_image, actual_features in query_data]
+
+    total_results = 0
+    correct_results = 0
+    false_positives = []
+    false_negatives = []
+    # main loop to match query images to training data
+    for future in futures_query:
+        segments_kp_desc, (path, actual_features) = future.result()
+        if len(segments_kp_desc) == 0:
+            print(f'No descriptors found for {path}')
+            continue
+
+        query_image = cv2.imread(path)
+
+        predicted_features = []
+        for (query_kp, query_desc), bounding_box in segments_kp_desc:
+            for feature_name, train_kp, train_desc in all_training_data_kp_desc:
+                matches = bf.knnMatch(query_desc, train_desc, k=2)
+
+                # filter out matches using Lowe's ratio test
+                good_matches = []
+                for match in matches:
+                    if len(match) == 2:
+                        m, n = match
+                        if m.distance < params['ratioThreshold'] * n.distance:
+                            good_matches.append(m)
+
+                # at least 4 matches are needed for homography
+                if len(good_matches) < 4:
+                    continue
+
+                # extract source (query) and destination (train) keypoints coordinates from good matches
+                src_pts = np.float32([query_kp[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+                dst_pts = np.float32([train_kp[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+
+                # find homography matrix between source and destination points using RANSAC
+                _, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, **params['RANSAC'])
+                matches_mask = mask.ravel().tolist()
+
+                # it is considered a match if there are than 'inlierScore' inliers
+                if sum(matches_mask) > params['inlierScore']:
+                    draw_bounding_box(query_image, bounding_box, feature_name)
+                    predicted_features.append(feature_name)
+                    correct_results += 1
+            total_results += 1
+
+        predicted_feature_names_set = set(predicted_features)
+        actual_feature_names_set = set([f[0] for f in actual_features])
+
+        # calculate accuracy
+        correct_predictions = len(predicted_feature_names_set.intersection(actual_feature_names_set))
+        total_features = len(actual_feature_names_set)
+        accuracy = correct_predictions / total_features
+        print(f"Accuracy ({path}): {accuracy}")
+
+        # calculate false positives and false negatives
+        false_pos_diff = predicted_feature_names_set.difference(actual_feature_names_set)
+        false_neg_diff = actual_feature_names_set.difference(predicted_feature_names_set)
+        false_positives += list(false_pos_diff)
+        false_negatives += list(false_neg_diff)
+
+        if show_output:
+            cv2.imshow('image', query_image)
+            cv2.waitKey(0)
+
+    end_time = time.time()
+
+    print('\nSummary of results:')
+    print(f'False positives ({len(false_positives)}): {Counter(false_positives)}')
+    print(f'False negatives ({len(false_negatives)}):, {Counter(false_negatives)}')
+    print(f'Final accuracy: {round(correct_results/total_results, 2)}')
+    print(f'Avg. time per query image: {round((end_time - start_time) / len(query_data), 2)} seconds')
+
+    if show_output:
+        cv2.destroyAllWindows()
 
 
 def main_process_for_marker(test_images_and_features, training_images_and_paths, params, show_output=False):
@@ -222,12 +427,11 @@ def feature_detection_marker(sift, bf, gray_query_image, colour_query_image, all
     return found_features, colour_query_image, run_time
 
 
-def read_test_dataset(dir, file_ext, read_colour=False):
+def read_test_dataset(dir, file_ext):
     print(f'Reading test dataset: {dir}')
 
     image_files = os.listdir(dir + 'images/')
-    image_files = sorted(image_files, key=lambda x: int(
-        x.split("_")[2].split(".")[0]))
+    image_files = sorted(image_files, key=lambda x: int(x.split("_")[2].split(".")[0]))
 
     all_data = []
     for image_file in image_files:
@@ -245,27 +449,25 @@ def read_test_dataset(dir, file_ext, read_colour=False):
             all_features.append([feature_class, feature_coord1, feature_coord2])
         path = dir + 'images/' + image_file
 
-        if read_colour:
-            all_data.append((
-                cv2.imread(path, cv2.IMREAD_GRAYSCALE),
-                cv2.imread(path, cv2.IMREAD_COLOR),
-                all_features
-            ))
-        else:
-            all_data.append((
-                cv2.imread(path, cv2.IMREAD_GRAYSCALE),
-                all_features
-            ))
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+
+        all_data.append((
+            path,
+            img,
+            all_features
+        ))
 
     return all_data
 
 
 def read_training_dataset(dir):
     print(f'Reading training dataset: {dir}')
-    return [(
-        cv2.imread(dir + 'png/' + path, cv2.IMREAD_GRAYSCALE),
-        path
-    ) for path in os.listdir(dir + 'png/')]
+    training_data = []
+    for path in os.listdir(dir + 'png/'):
+        img = cv2.imread(dir + 'png/' + path, cv2.IMREAD_GRAYSCALE)
+        feature_name = feature_name_from_path(path)
+        training_data.append((img, feature_name))
+    return training_data
 
 
 def remove_noise_from_image(image, kernel=np.ones((3, 3), np.uint8)):
--- Task3/marker_test.py
+++ Task3/marker_test.py
@@ -1,10 +1,41 @@
 import traceback
 
-from main import main_process_for_marker, read_test_dataset, read_training_dataset
-
+from main import feature_detection, read_test_dataset, read_training_dataset
 
 RED = '\u001b[31m'
 NORMAL = '\u001b[0m'
+
+
+if __name__ == '__main__':
+    params = {'RANSAC': {'ransacReprojThreshold': 11.110294305510669},
+              'SIFT': {'contrastThreshold': 0.0039052330228148877,
+                       'edgeThreshold': 16.379139206562137,
+                       'nOctaveLayers': 6,
+                       'nfeatures': 1700,
+                       'sigma': 2.2201211013686857},
+              'BF': {'crossCheck': False,
+                     'normType': 2},
+              'inlierScore': 4,
+              'ratioThreshold': 0.6514343913409797,
+              'resizeQuery': 95}
+
+    # NOTE: For marker, we have assumed that the additional data you have is in the same format as the data given.
+    # Please replace the below three directories with your own and then execute this file.
+    train_data_dir = 'Task2Dataset/Training/'
+    query_data_dir = 'Task3Dataset/'
+
+    try:
+        train_data = read_training_dataset(train_data_dir)
+        query_data = read_test_dataset(query_data_dir, '.csv')
+    except Exception as e:
+        print(RED, 'Error while reading datasets:', NORMAL, traceback.format_exc())
+        exit()
+
+    # set the show_output to True to see the query images with the detected features
+    # if set to true ignore 'Avg. time per query image' metric
+    feature_detection(train_data, query_data, params, show_output=False)
+
+"""
 
 if __name__ == '__main__':
     params = {
@@ -51,3 +82,7 @@ if __name__ == '__main__':
         params,
         show_output=False # TODO: set this to true before submitting
     )
+
+"""
+
+
